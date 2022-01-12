e336b6c232de8f4d19d1201e2ed702e83cc6d021
Abeljo <abeljo86@gmail.com>
2022-01-12 16:33:39 +0300

    add via git

---
+++ lib/login.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+                width: 100,
+                height: 100,
+                color: Colors.deepOrange,
+                child: Text(
+                  "hello",
+                  style: TextStyle(fontSize: 25, color: Colors.black),
+                )),
+            Row(children: [
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.lightBlue,
+                  child: Text(
+                    "hello",
+                    style: TextStyle(fontSize: 25, color: Colors.black),
+                  )),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.lightGreen,
+              ),
+            ]),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.black,
+            ),
+            Row(children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.lightGreenAccent,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.deepOrange,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.pink,
+              ),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
